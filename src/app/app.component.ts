9081c1bfef8b7e1aa1d88c354c50288aa5aad7f2
acoria <agusignaciocoria@gmail.com>
2022-08-29 16:19:53 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'edades';
+
+  edades: number[] = [];
+  promedio: number | undefined;
+
+  calcularPromedio(){ 
+    this.promedio = this.edades.reduce((avg,edad,_,{length}) => {
+      return avg + edad / length;
+    },0);
+  }
+
+  resetearTodas(){
+    this.edades.length = 0;
+    this.promedio = undefined;
+  }
+
+  mostrarPromedio(){
+    if(this.edades.length >= 2 && this.edades.every(x => x > 0)){
+      this.calcularPromedio();
+    } else {
+      alert("Edades inválidas. Ingrese dos enteros mayores a 0");
+    }
+  }
 }
